e7091355f86e6068985ba4d83caffdbb60fa23ce
AnjalBam <anjalbam81@gmail.com>
2020-08-25 22:24:36 +0545

    feat: initial setup app

---
+++ src/components/About/AboutPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This app uses REact REdux, adnd REact router.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
